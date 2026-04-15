6bd2b5d5d85fdb04994c55b42002446094660564
Manux <manuldestevez@gmail.com>
2026-04-15 20:36:13 +0200

    Fixed init to blazin

--- songs/blazin/modchart.hx
+++ songs/blazin/modchart.hx
@@ -1,7 +1,7 @@
 function onCreate() {
 	var mc = modChart; // alias corto
 
-	mc.addEventSimple(1, "cpu", -1, ALPHA, 0, 0, INSTANT);
+	mc.addEventSimple(-100, "cpu", -1, ALPHA, 0, 0, INSTANT);
 
-	mc.addEventSimple(1, "player", -1, MOVE_X, -300, 0, INSTANT);
+	mc.addEventSimple(-100, "player", -1, MOVE_X, -300, 0, INSTANT);
 }
