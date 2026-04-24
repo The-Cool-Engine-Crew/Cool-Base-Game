bafa075f3bb1c2cf776551efbdd82415aad2d1eb
Manux <manuldestevez@gmail.com>
2026-04-25 00:09:47 +0200

    More accuracy for tutorial in cam

--- songs/tutorial/scripts/gf.hx
+++ songs/tutorial/scripts/gf.hx
@@ -3,3 +3,11 @@ function postCreate()
     gf.visible = false;
     dad.x += 200;
 }
+
+function onBeatHit()
+{
+    if (cameraController.currentTarget == 'opponent')
+    {
+        FlxTween.tween(FlxG.camera, {zoom: 1}, (Conductor.stepCrochet * 4 / 1000), {ease: FlxEase.elasticInOut});
+    }
+}
